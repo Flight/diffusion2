9d3238d59b5cada217d497efc796958cc34dcd7d
Flight <flight.neg@gmail.com>
2014-06-10 23:46:26 +0300

    Logic done

--- js/common.js
+++ js/common.js
@@ -7,7 +7,8 @@
 			AD,																// koefficient difuzii pershoi domishki, m2/s
 			BD,																// koefficient difuzii drugoi domishki, m2/s
 			canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight),
-			c_prev, c_next, c_full;
+			APR,															// masiv pershoi domishki
+			BPR;															// masiv drugoi domishki
 
 		// Stvoruemo ob'ekt dlay zberigannya
 		$.storage = new $.store();
@@ -183,6 +184,19 @@
 
 		$('#param_TS, #param_AName, #param_BName').bind('change', updateDiffusion).change();
 
+		function deepCopy(o) {
+			var copy = o,k;
+
+			if (o && typeof o === 'object') {
+				copy = Object.prototype.toString.call(o) === '[object Array]' ? [] : {};
+				for (k in o) {
+					copy[k] = deepCopy(o[k]);
+				}
+			}
+
+			return copy;
+		}
+
 		function evaluate() {
 			var T = getParam('T'),					// chas processu, sec
 				W = getParam('W'),					// shirina plastini, m
@@ -206,10 +220,15 @@
 				nx = W/dl,							// kilkist intervaliv po shiriny
 				ny = H/dl,							// kilkist intervaliv po glubiny
 				G = Math.max(AD, BD)*dt/(dl*dl),	// bezrozmirna difuziya
-				P,									// kilkist promizhnyh tochok po chasu
+				Gp = G,								// bezrozmirna difuziya dlya rozrahunkovoi sitki
+				P = 1,								// kilkist promizhnyh tochok po chasu
 				deltaP,								// krok rozrahunkovoi shemi po chasu
+				ANU = [],							// masiv pochatkovih umov pershoi domishki
+				BNU = [],							// masiv pochatkovih umov drugoi domishki
 				AGU = [],							// masiv granichnih umov pershoi domishki
-				BGU = [];							// masiv granichnih umov drugoi domishki
+				BGU = [],							// masiv granichnih umov drugoi domishki
+				Aprev = [],							// masiv poperednyogo slou pershoi domishki
+				Bprev = [];							// masiv poperednyogo slou drugoi domishki
 
 			if ( BSpaces.length && BD > AD ) {
 				DMax = BD;
@@ -222,11 +241,46 @@
 			if (dt > deltaMax) {
 				P = dt/deltaMax;
 				deltaP = dt/P;
+				Gp = G/P;
 			}
+
+			console.log( 'Gp:' );
+			console.log( Gp );
 
 			for (var i = 0; i <= nx; i++) {
 				AGU[i] = 0;
 				BGU[i] = 0;
+			}
+				
+			for (var j = 0; j <= ny; j++) {
+				ANU[j] = [];
+				BNU[j] = [];
+
+				Aprev[j] = [];
+				Bprev[j] = [];
+
+				for (var i = 0; i <= nx; i++) {
+					ANU[j][i] = 0;
+					BNU[j][i] = 0;
+					
+					Aprev[j][i] = 0;
+					Bprev[j][i] = 0;
+				}
+			}
+			
+			if (!APR) {
+				APR = [];
+				BPR = [];
+
+				for (var j = 0; j <= ny; j++) {
+					APR[j] = [];
+					BPR[j] = [];
+
+					for (var i = 0; i <= nx; i++) {
+						APR[j][i] = 0;
+						BPR[j][i] = 0;
+					}
+				}
 			}
 
 			ASpaces.forEach(function(ASpace) {
@@ -240,10 +294,74 @@
 				}
 			});
 
+			console.log( 'AGU:' );
 			console.log( AGU );
 
+			for (var i = 0; i <= nx; i++) {
+				ANU[0][i] = AGU[i];
+				BNU[0][i] = BGU[i];
+			}
 
-			return;
+			console.log( 'ANU[0]:' );
+			console.log( ANU[0] );
+
+			if (!usePrev) {
+				APR = deepCopy(ANU);
+				BPR = deepCopy(BNU);
+			}
+
+			console.log( 'APR[0]:' );
+			console.log( APR[0] );
+
+			for (var k=0; k < P; k++) {
+				Aprev = deepCopy(APR);
+				Bprev = deepCopy(BPR);
+
+				console.log( 'Aprev[0]:' );
+				console.log( Aprev[0] );
+
+				for (var i = 0; i <= nx; i++) {		// Verhnya ta nizhnya stinki
+					if (AGU[i] > 0) {
+						APR[0][i] = AGU[i];			// umova pershogo rody
+					} else {
+						APR[0][i] = Aprev[1][i];	// umova drugogo rody
+					}
+					APR[ny][i] = Aprev[ny-1][i];	// umova drugogo rody
+
+					if (BGU[i] > 0) {
+						BPR[0][i] = BGU[i];			// umova pershogo rody
+					} else {
+						BPR[0][i] = Bprev[1][i];	// umova drugogo rody
+					}
+					BPR[ny][i] = Bprev[ny-1][i];	// umova drugogo rody
+				};
+
+				console.log( 'Aprev[0][1]:' );
+				console.log( Aprev[0][1] );
+
+				for (var j = 0; j <= ny; j++) {		// Liva ta prava stinki
+					APR[j][0] = Aprev[j][1];		// umova drugogo rody
+					APR[j][nx] = Aprev[j][nx-1];	// umova drugogo rody
+
+					BPR[j][0] = Bprev[j][1];		// umova drugogo rody
+					BPR[j][nx] = Bprev[j][nx-1];	// umova drugogo rody
+				};
+
+				console.log( 'APR[0][0]:' );
+				console.log( APR[0][0] );
+
+				for (var j = 1; j <= ny-1; j++) {
+					for (var i = 1; i <= nx-1; i++) {
+						APR[j][i] = ( 1-4*Gp )*Aprev[j][i] + Gp*( Aprev[j-1][i] + Aprev[j][i-1] + Aprev[j][i+1] + Aprev[j+1][i] );
+						BPR[j][i] = ( 1-4*Gp )*Bprev[j][i] + Gp*( Bprev[j-1][i] + Bprev[j][i-1] + Bprev[j][i+1] + Bprev[j+1][i] );
+					}
+				}
+
+				if (k = 1) {
+					console.log( 'APR[1]:' );
+					console.log( APR[1] );
+				}
+			}
 
 			console.log('usePrev: ' + usePrev);
 			console.log('Безрозмірна дифузія: ' + G);
@@ -255,209 +373,27 @@
 				return;
 			}
 
-			if (G > 0.5) {
-				if (G > 1000) {
-					alert('Ця операція не може бути виконана за розумний час!');
-					return;
-				} else if (G > 100) {
-					if (!confirm('Ця операція займе багато часу, ві впевнені, що хочете продовжити?')) {
-						return;
-					}
-				}
-				console.log('Безрозмірна дифузія надто велика, вводимо проміжні точки по часу');
-				m = Math.ceil(2*G);
-				console.log('Проміжна точка по часу:' + m);
-				G = G/m;
-				console.log('Безрозмірна дифузія: ' + G);
-			}
-
-			console.log(c_prev);
-			if (!usePrev || !c_prev) {
-				c_prev = new Array(n+1);
-				c_next = new Array(n+1);
-				c_full = new Array(k+1);
-
-				for (var t = 0; t < k+1; t++) {
-					c_full[t] = new Array(n+1);
-				}
-
-				// Pochatkovi umovy
-				for (var i = 0; i < n+1; i++) {
-					c_prev[i] = 0;
-				}
-			} else {
-				console.log('Використовуємо попередні значення! ' + c_prev[0])
-				for (var i = 0; i < n+1; i++) {
-					c_full[0][i] = c_prev[i];
-				}
-			}
-
-			if (sk) {
-				boundTypeLeft = 2;
-			}
-
-			// Granichni umovy
-			switch (boundTypeLeft) {
-				case 1:
-					c_prev[0] = C1m;
-					break;
-				case 2:
-					c_prev[0] = c_prev[1];
-					break;
-				case 3:
-					if (typeLeft3 == 2) {
-						c_prev[0] = c_prev[1];
-					}
-					if (typeLeft3 == 1) {
-						var dQL = (c_prev[0] - c_prev[1])*G*dl;		// Vitrata leguuchoi prisadki dlya umov pershogo rodu
-						if (dQL < 0) {								// Yaksho vytraty prisadki videmni
-							c_prev[0] = c_prev[1];					// Priminyaemo umovy drugogo rodu
-							typeLeft3 = 2;							//
-						} else {									// Yaksho vytraty prisadki dodatni
-							if (QL+dQL < Q1) {						// Yaksho prisadki vistachae
-								c_prev[0] = C1m;					// Granichni umovi pershogo rodu
-								QL += dQL;							// Sumarna vitrata leguuchoi prisadki
-							} else if (QL < Q1) {					// Yaksho prisadki ne vistachae
-								c_prev[0] -= (Q1 - QL)/(2*G * dl);	// Pererahovuemo concentracii vyhodyachi z realnoi kilkosti prisadki
-								c_prev[1] = c_prev[0];				// Priminyaemo umovy drugogo rodu
-								typeLeft3 = 2;						//
-							}
-						}
-					}
-					break;
-				default:
-					break;
-			}
-			switch (boundTypeRight) {
-				case 1:
-					c_prev[n] = C2m;
-					break;
-				case 2:
-					c_prev[n] = c_prev[n-1];
-					break;
-				case 3:
-					if (typeRight3 == 2) {
-						c_prev[n] = c_prev[n-1];
-					}
-					if (typeRight3 == 1) {
-						var dQR = (c_prev[n] - c_prev[n-1])*G*dl;	// Vitrata leguuchoi prisadki dlya umov pershogo rodu
-						if (dQR < 0) {								// Yaksho vytraty prisadki videmni
-							c_prev[n] = c_prev[n-1];				// Priminyaemo umovy drugogo rodu
-							typeRight3 = 2;							//
-						} else {									// Yaksho vytraty prisadki dodatni
-							if (QR+dQR < Q2) {						// Yaksho prisadki vistachae
-								c_prev[n] = C2m;					// Granichni umovi pershogo rodu
-								QR += dQR;							// Sumarna vitrata leguuchoi prisadki
-							} else if (QR < Q2) {					// Yaksho prisadki ne vistachae
-								c_prev[n] -= (Q2 - QR)/(2*G * dl);	// Pererahovuemo concentracii vyhodyachi z realnoi kilkosti prisadki
-								c_prev[n-1] = c_prev[n];			// Priminyaemo umovy drugogo rodu
-								typeRight3 = 2;						//
-							}
-						}
-					}
-					break;
-				default:
-					break;
-			}
-
-			if (!usePrev) {
-				for (var i = 0; i < n+1; i++) {
-					c_full[0][i] = c_prev[i];
-				}
-			}
-
-			QL = 0;
-			QR = 0;
-			typeLeft3 = 1;							// Pochatkoviy stan dlya kombinovanogo tipu umov L
-			typeRight3 = 1;							// Pochatkoviy stan dlya kombinovanogo tipu umov R
-			for (var t = 1; t <= k; t++) { 			// Cikl po chasu dlya zadanih tochok
-				for (var p = 0; p < m; p++) {		// Cikl po chasu dlya promizhnih tochok
-					for (var i = 1; i < n; i++) {	// Cikl po koordinati dlya zadanih tochok
-						if (sk) {
-							c_next[i] = G*(2*i+1)/(2*i)*c_prev[i-1] + (1-2*G)*c_prev[i] + G*(2*i-1)/(2*i)*c_prev[i+1];	// Rizniceva shema dlya drugogo zakonu Fika
-						} else {
-							c_next[i] = G*c_prev[i-1] + (1-2*G)*c_prev[i] + G*c_prev[i+1];	// Rizniceva shema dlya drugogo zakonu Fika
-						}
-					}
-
-					switch (boundTypeLeft) {
-						case 1:
-							c_next[0] = C1m;
-							break;
-						case 2:
-							c_next[0] = c_next[1];
-							break;
-						case 3:
-							if (typeLeft3 == 2) {
-								c_next[0] = c_next[1];
-							}
-							if (typeLeft3 == 1) {
-								var dQL = (c_prev[0] - c_prev[1])*G*dl;		// Vitrata leguuchoi prisadki dlya umov pershogo rodu
-								if (dQL < 0) {								// Yaksho vytraty prisadki videmni
-									c_next[0] = c_next[1];					// Priminyaemo umovy drugogo rodu
-									typeLeft3 = 2;
-								} else {									// Yaksho vytraty prisadki dodatni
-									if (QL+dQL < Q1) {						// Yaksho prisadki vistachae
-										c_next[0] = C1m;					// Granichni umovi pershogo rodu
-										QL += dQL;							// Sumarna vitrata leguuchoi prisadki
-									} else if (QL < Q1) {					// Yaksho prisadki ne vistachae
-										c_next[0] -= (Q1 - QL)/(2*G * dl);	// Pererahovuemo concentracii vyhodyachi z realnoi kilkosti prisadki
-										c_next[1] = c_next[0];				// Priminyaemo umovy drugogo rodu
-										typeLeft3 = 2;						//
-									}
-								}
-							}
-							break;
-						default:
-							break;
-					}
-
-					switch (boundTypeRight) {
-						case 1:
-							c_next[n] = C2m;
-							break;
-						case 2:
-							c_next[n] = c_next[n-1];
-							break;
-						case 3:
-							if (typeRight3 == 2) {
-								c_next[n] = c_next[n-1];
-							}
-							if (typeRight3 == 1) {
-								var dQR = (c_prev[n] - c_prev[n-1])*G*dl;	// Vitrata leguuchoi prisadki dlya umov pershogo rodu
-								if (dQR < 0) {								// Yaksho vytraty prisadki videmni
-									c_next[n] = c_next[n-1];				// Priminyaemo umovy drugogo rodu
-									typeRight3 = 2;							//
-								} else {									// Yaksho vytraty prisadki dodatni
-									if (QR+dQR < Q2) {						// Yaksho prisadki vistachae
-										c_next[n] = C2m;					// Granichni umovi pershogo rodu
-										QR += dQR;							// Sumarna vitrata leguuchoi prisadki
-									} else if (QR < Q2) {					// Yaksho prisadki ne vistachae
-										c_next[n] -= (Q2 - QR)/(2*G * dl);	// Pererahovuemo concentracii vyhodyachi z realnoi kilkosti prisadki
-										c_next[n-1] = c_next[n];			// Priminyaemo umovy drugogo rodu
-										typeRight3 = 2;						//
-									}
-								}
-							}
-							break;
-						default:
-							break;
-					}
-
-					for (var i = 0; i < n+1; i++) {
-						c_prev[i] = c_next[i];
-					}
-				}
-				for (var i = 0; i < n+1; i++) {		// Zapamyataly masiv na potochniy chas
-					c_full[t][i] = c_prev[i];
-				}
-			}
+			// if (G > 0.5) {
+			// 	if (G > 1000) {
+			// 		alert('Ця операція не може бути виконана за розумний час!');
+			// 		return;
+			// 	} else if (G > 100) {
+			// 		if (!confirm('Ця операція займе багато часу, ві впевнені, що хочете продовжити?')) {
+			// 			return;
+			// 		}
+			// 	}
+			// 	console.log('Безрозмірна дифузія надто велика, вводимо проміжні точки по часу');
+			// 	m = Math.ceil(2*G);
+			// 	console.log('Проміжна точка по часу:' + m);
+			// 	G = G/m;
+			// 	console.log('Безрозмірна дифузія: ' + G);
+			// }
 
 			var c_max = 0;
-			for (var t = 0; t < k+1; t++) {
-				for (var i = 0; i < n+1; i++) {
-					if (c_full[t][i] > c_max) {
-						c_max = c_full[t][i];
+			for (var t = 0; t <= ny; t++) {
+				for (var i = 0; i <= nx; i++) {
+					if (APR[t][i] > c_max) {
+						c_max = APR[t][i];
 					}
 				}
 			}
@@ -466,12 +402,12 @@
 			var c_left = null,
 				c_right = null,
 				c_current = null;
-			for (var t = 0; t < k+1; t++) {
+			for (var t = 0; t <= nx; t++) {
 				c_extend[t] = new Array(canvasWidth);
-				c_extend[t][0] = c_full[t][0];
-				for (var i = 0; i < n+1; i++) {
-					c_left = c_full[t][i];
-					c_right = c_full[t][i+1];
+				c_extend[t][0] = APR[t][0];
+				for (var i = 0; i <= ny+1; i++) {
+					c_left = APR[t][i];
+					c_right = APR[t][i+1];
 					c_extend[t][i*xn] = c_left;
 					for (var xi = 1; xi < xn; xi++) {
 						c_current = (c_left*(xn-xi)+c_right*xi)/xn;
